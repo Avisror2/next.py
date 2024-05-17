b4a48ba969143b8edf14d56957f69e9fb12257fb
Avisror2 <avisrorofek@gmail.com>
2024-05-17 18:34:31 +0300

    Unit 1
    Added solution for question 4 and organized code
    
    stage_4 creates a file called name_length and writes to the file the name lengths of all names in name file

--- Unit1.py
+++ Unit1.py
@@ -13,16 +13,28 @@ def stage_3(name_file):
     print("\n".join(name for name in names if len(name) == min_length))
 
 
+#  Creates a new file called name_length and writes all the name lengths to it from name file
+def stage_4(name_file):
+    with open('name_length.txt', "w+") as name_length_file:
+        for name in name_file.read().split():
+            name_length_file.write('{}\n'.format(name))
+
+
 def main():
 
     with open(r'C:\Users\user\PycharmProjects\next\names.txt') as name_file:
-        print(stage_1(name_file))
+
+        print(stage_1(name_file))  # Stage 1
         name_file.seek(0)  # Reset the pointer back after each stage
-        print(stage_2(name_file))
-        name_file.seek(0)  # Reset the pointer back after each stage
-        stage_3(name_file)
-        name_file.seek(0)  # Reset the pointer back after each stage
-        stage_3(name_file)
+
+        print(stage_2(name_file))  # Stage 2
+        name_file.seek(0)
+
+        stage_3(name_file)  # Stage 3
+        name_file.seek(0)
+
+        stage_4(name_file)  # Stage 4
+        name_file.seek(0)
 
 
 if __name__ == '__main__':
